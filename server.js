2346c61845861c7ef8e77679f2d639297f13e10b
Shakhan <khan.vinn@gmail.com>
2021-07-15 17:56:58 +0500

    basic routes and file uploading api

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer({ dest: 'uploads/' })
+
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res)=>{
+  console.log(req.file)
+  res.json({})
+})
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });

540d452ae24287e786684e58c6b3c75a279d5344
Shakhan <khan.vinn@gmail.com>
2021-07-15 18:07:49 +0500

    any properties returned about file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var cors = require("cors");
 var multer = require("multer");
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: "uploads/" });
 
 require("dotenv").config();
 
@@ -10,10 +10,14 @@ var app = express();
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
-app.post("/api/fileanalyse", upload.single('upfile'), (req,res)=>{
-  console.log(req.file)
-  res.json({})
-})
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
+
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
